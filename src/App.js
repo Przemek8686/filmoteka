9438e5ee14a073856a8812411e961e726f4164a0
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-06-05 20:52:25 +0200

    initial tile and stylization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import './App.css';
 import { Container } from './components/Container';
+import {
+  Tile,
+  TileContent as Content,
+  TileDescription as Description,
+  TileImage as Image,
+  TileTag as Tag,
+  TileTags as Tags,
+  TileTitle as Title,
+  TileSubtitle as Subtitle
+} from './components/Tile';
+import poster from "./images/poster.png";
 
 function App() {
   return (
     <Container>
-      example textbox content
+      <Tile>
+        <Image src={poster} alt="" />
+        <Content>
+          <Title>Mulan</Title>
+          <Subtitle>2020</Subtitle>
+          <Tags>
+            <Tag>Action</Tag>
+            <Tag>Action</Tag>
+            <Tag>Action</Tag>
+          </Tags>
+          <Description>A young Chinese maiden disguises herself as a male warrior in order to save her father.
+            Disguises herself as a male warrior in order to save her father.  A young Chinese maiden disguises herself as a male warrior in order to save her father.</Description>
+        </Content>
+      </Tile>
     </Container>
   );
 }
--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -1,4 +1,48 @@
 import { styled } from "styled-components";
 
 export const Tile = styled.article`
+    background: ${({ theme }) => theme.color.white};
+    padding: 40px;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 40px;
+`;
+
+export const TileImage = styled.img`
+    width: 312px;
+    height: 464px;
+`;
+
+export const TileContent = styled.div``;
+
+export const TileTitle = styled.header`
+    font-weight: 600;
+    font-size: 36px;
+    margin-bottom: 24px;
+`;
+
+export const TileSubtitle = styled.div`
+    font-size: 22px;
+`;
+
+export const TileTags = styled.ul`
+    padding-left: 0px;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+    margin: 16px -8px;
+`;
+
+export const TileTag = styled.li`
+    background: ${({ theme }) => theme.color.mystic};
+    padding: 8px 16px;
+    font-size: 14px;
+    margin: 8px;
+    border-radius: 5px;
+`;
+
+export const TileDescription = styled.p`
+    font-size: 20px;
+    margin: 24px 0 0 0;
+    line-height: 1.6;
 `;
