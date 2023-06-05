b4b785dcc0857903f2db0ff3451b92f13e4157ef
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-06-05 15:34:40 +0200

    not good code

---
+++ src/components/Container/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 1368px;
+    margin: auto;
+    background: #FFFFFF;
+    box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
+    width: 1368px;
+    height: 544px;
+`;
