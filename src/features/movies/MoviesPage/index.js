81da9ce56ae71a2682425cb31b8ccfc0d98b5e51
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-06-19 20:19:41 +0200

    structure v2 initiated

---
+++ src/features/movies/MoviesPage/index.js
@@ -0,0 +1,13 @@
+function MoviesPage() {
+
+  return (
+    <>
+      <div>Movie1</div>
+      <div>Movie2</div>
+      <div>Movie3</div>
+      <div>Movie4</div>
+      <div>Movie5</div>
+      <div>Movie6</div>
+    </>
+  );
+};
